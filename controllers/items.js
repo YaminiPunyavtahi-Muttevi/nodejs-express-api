0f05de822ec6e9d395851f44582e6f5e8d8f7809
Yamini Muttevi <yamini_muttevi@epam.com>
2024-11-11 11:55:47 +0530

    updated to JSON format.

--- controllers/items.js
+++ controllers/items.js
@@ -13,31 +13,31 @@ const addItem = (req, res) => {
 };
 
 const getItemId = (req, res) => {
-  const { id } = req.params;
-  const itemFound = itemData.find((item) => item.id === id);
+ // const { id } = req.params;
+  //const itemFound = itemData.find((item) => item.id === id);
   console.log(itemFound);
   res.send(itemFound);
 };
 
 const deleteItem = (req, res) => {
-  const { id } = req.params;
-  itemData = itemData.filter((item) => item.id !== id);
+  //const { id } = req.params;
+  //itemData = itemData.filter((item) => item.id !== id);
   console.log(itemData);
   res.send(`Item with id ${id} has been deleted successfully.`);
 };
 
 const updateItem = (req, res) => {
-  const { id } = req.params;
+  //const { id } = req.params;
   const { label, value } = req.body;
 
-  const item = itemData.find((item) => item.id === id);
+  //const item = itemData.find((item) => item.id === id);
 
   if (label) item.label = label;
   if (value) item.value = value;
   
 
   res.send(
-    `Item with id ${id} and name ${item.name} has been updated successfully`
+    `Item with name ${item.name} has been updated successfully`
   );
 };
 
