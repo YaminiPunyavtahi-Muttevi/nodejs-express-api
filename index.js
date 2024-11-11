8278e859c72c8df84b610f155bb838ab8af8ef4e
Yamini Muttevi <yamini_muttevi@epam.com>
2024-11-11 13:47:15 +0530

    updated url.

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use("/apidata", apiRoutes);
 app.use(function (req, res, next) {
 
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'https://nodejs-express-api-brown.vercel.app/');
+  res.setHeader('Access-Control-Allow-Origin', 'https://forms.sitecorecloud.io');
 
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
